3c92467b7a36f6cadfd0b055cc4cccc8f71b228a
khandelwalpranav05 <khandelwalpranav@rocketmail.com>
2019-09-16 21:39:06 +0530

    bitmasking

--- Lecture 10/bitmasking_questions.cpp
+++ Lecture 10/bitmasking_questions.cpp
@@ -2,38 +2,43 @@
 
 using namespace std;
 
-int uniqueNumberPair(int arr[],int n){
+void uniqueNumberPair(int arr[],int n){
 
 	int pairXor = arr[0];
 
 	for(int i=1;i<n;i++){
-
+		pairXor = pairXor^arr[i];
 	}
 
 	int var = pairXor;
-	int position = 0
+	int position = 0;
 
-	while(){
-		int temp = var&1;
-
-		if(temp==1){
-			//
+	while(true){
+		if(var&1){
+			break;
 		}else{
 			var = var>>1;
 			position++;
 		}
 	}
 
-	for(int i;i<;i++){
+
+	int left_unique = 0;
+
+	for(int i=0;i<n;i++){
 
 		int element = arr[i];
 		element = element>>position;
 
 		if(element&1){
-
+			left_unique = left_unique^arr[i];
 		}
-
 	}
+
+	int right_unique = left_unique^pairXor;
+
+	cout<<"First unique Value "<<left_unique<<endl;
+	cout<<"Second unique Value "<<right_unique<<endl;
 }
 
 int uniqueNumber(int arr[],int n){
@@ -65,10 +70,12 @@ int main(){
 	// int arr[] = {1,2,3,4,5,6,7,6,5,4,3,2,1};
 	// cout<<uniqueNumber(arr,13)<<endl;
 
-	cout<<countSetBits(13)<<endl;
+	// cout<<countSetBits(13)<<endl;
 
 	// int arr[] = {1,1,3,3,4,5,4,8,8,7};
-	// cout<<uniqueNumberPair(arr,12)<<endl;
+	// uniqueNumberPair(arr,10);
+
+	int arr[] = {1,1,2,2,3,5,6,7,6,5,6,5,3,1,2,3}
 
 	return 0;
 }
