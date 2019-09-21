874d98b7f1e806b952a4ae1e1e34a679fc8ce6db
khandelwalpranav05 <pranav.khandelwal@lifcare.in>
2019-09-21 09:46:58 +0530

    recursion

--- Lecture 11/recursion_basics.cpp
+++ Lecture 11/recursion_basics.cpp
@@ -22,13 +22,45 @@ int power(int n,int p){
 	return power_n;
 }
 
+int sumTillN(int n){
+	if(n==0){
+		return 0;
+	}
+
+	// if(n==1){
+	// 	return 1;
+	// }
+
+	//Recursive case
+	int sum_n1 = sumTillN(n-1);
+	int sum = sum_n1 + n;
+	return sum;
+}
+
+void printD(int n){
+	// if(n==1){
+	// 	cout<<1<<endl;
+	// 	return;
+	// }
+
+	if(n==0){
+		return;
+	}
+
+	cout<<n<<endl;
+	printD(n-1);
+	return;
+}
+
 int main(){
 
 	// cout<<factorial(5)<<endl;
 
 	// cout<<power(2,8)<<endl;
 
-	
+	// cout<<sumTillN(10)<<endl;
+
+	printD(5);
 
 	return 0;
 }
