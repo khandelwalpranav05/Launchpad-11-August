04d4ebfa775a39ecebdb917701f8f10f330cefa4
khandelwalpranav05 <pranav.khandelwal@lifcare.in>
2019-09-21 10:42:31 +0530

    recursion basics

--- Lecture 11/recursion_basics.cpp
+++ Lecture 11/recursion_basics.cpp
@@ -52,6 +52,48 @@ void printD(int n){
 	return;
 }
 
+void printI(int n){
+	if(n==0){
+		return;
+	}
+
+	printI(n-1);
+	cout<<n<<endl;
+	return;
+
+}
+
+void printDI(int n){
+	if(n==0){
+		return;
+	}
+
+	cout<<n<<endl;
+	printDI(n-1);
+	cout<<n<<endl;
+	return;
+}
+
+bool isSorted(int arr[],int idx,int n){
+	//Base Case
+	if(idx==n-1){
+		return true;
+	}
+
+	//Recursive Case
+	bool rest_of_the_array = isSorted(arr,idx+1,n);
+
+	if(rest_of_the_array==false){
+		return false;
+	}else{
+		if(arr[idx]>arr[idx+1]){
+			return false;
+		}else{
+			return true;
+		}
+	}
+}
+
 int main(){
 
 	// cout<<factorial(5)<<endl;
@@ -60,7 +102,19 @@ int main(){
 
 	// cout<<sumTillN(10)<<endl;
 
-	printD(5);
+	// printD(5);
+	// printI(5);
+	// cout<<"*********"<<endl;
+	// printDI(5);
+
+	int arr[] = {1,2,3,4,5};
+	int brr[] = {132,2,3,5,6};
+
+	cout<<isSorted(arr,0,5)<<endl;
+	cout<<isSorted(brr,0,5)<<endl;
+
+	// cout<<sizeof(printDI(5))<<endl;
+	
 
 	return 0;
 }
