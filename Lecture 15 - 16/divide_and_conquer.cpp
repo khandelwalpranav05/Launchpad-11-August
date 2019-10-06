721571adaea3bd0c51eef593008e0c545527073c
khandelwalpranav05 <pranav.khandelwal@lifcare.in>
2019-10-06 10:57:25 +0530

    divide and conquer

--- Lecture 15 - 16/divide_and_conquer.cpp
+++ Lecture 15 - 16/divide_and_conquer.cpp
@@ -136,6 +136,120 @@ int rotatedArray(int arr[],int s,int e,int data){
 		}
 
 	}
+}
+
+int uniqueNumber(int arr[],int s,int e){
+	if(s>e){
+		return -1;
+	}
+
+	int mid = s + (e-s)/2;
+
+	if(arr[mid-1]!=arr[mid] and arr[mid+1]!=arr[mid]){
+		return mid;
+	}
+
+	if(mid&1){
+		if(arr[mid]==arr[mid-1]){
+			return uniqueNumber(arr,mid+1,e);
+		}else{
+			return uniqueNumber(arr,s,mid-1);
+		}
+	}else{
+		if(arr[mid]==arr[mid+1]){
+			return uniqueNumber(arr,mid+2,e);
+		}else{
+			return uniqueNumber(arr,s,mid-2);
+		}
+	}
+}
+
+void quickSort(int arr[],int low,int high){
+	if(low>=high){
+		return;
+	}
+
+	int mid = low + (high-low)/2;
+
+	int pivot = arr[mid];
+
+	int left = low;
+	int right = high;
+
+	while(left<=right){
+
+		while(arr[left]<pivot){
+			left++;
+		}
+
+		while(arr[right]>pivot){
+			right--;
+		}
+
+		if(left<=right){
+			swap(arr[left],arr[right]);
+			left++;
+			right--;
+		}
+
+	}
+
+	quickSort(arr,low,right);
+	quickSort(arr,left,high);
+
+}
+
+bool isValid(int arr[],int n,int k,int mid){
+
+	int painters = 1;
+	int painterTime = 0;
+
+	for(int i=0;i<n;i++){
+
+		painterTime+=arr[i];
+
+		if(painterTime>mid){
+			painters++;
+			painterTime = arr[i];
+
+			if(painters>k){
+				return false;
+			}
+
+		}
+
+	}
+
+	return true;
+
+}	
+
+int painterProblem(int arr[],int n,int k,int time=1){
+
+	int minTime = 40;
+
+	int maxTime = 130;
+
+	int ans = maxTime;
+
+	while(minTime<=maxTime){
+
+		int mid = minTime + (maxTime-minTime)/2;
+
+		if(isValid(arr,n,k,mid)){
+
+			ans = mid;
+			maxTime = mid-1;
+
+		}else{
+
+			minTime = mid+1;
+
+		}
+
+	}
+
+	return ans*time;
 
 }
 
@@ -154,8 +268,24 @@ int main(){
 	// cout<<lowerBound(arr,8,2)<<endl;
 
 
-	int arr[] = {5,6,7,1,2,3,4};
-	cout<<rotatedArray(arr,0,6,2)<<endl;
+	// int arr[] = {5,6,7,1,2,3,4};
+	// cout<<rotatedArray(arr,0,6,2)<<endl;
+
+
+	// int arr[] = {1,3,3,5,5,6,6,7,7,8,8};
+	// cout<<uniqueNumber(arr,0,10)<<endl;
+
+	// int arr[] = {7,6,5,4,3,2,1};
+	// quickSort(arr,0,6);
+
+	// for(int i=0;i<7;i++){
+	// 	cout<<arr[i]<<" ";
+	// }
+	// cout<<endl;
+
+	int arr[] = {25,30,35,40};
+	cout<<painterProblem(arr,4,2)<<endl;
+
 
 	// int x = 2147483647;
 	// cout<<x<<endl;
