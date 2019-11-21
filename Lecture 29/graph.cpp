4ccf35e02abde64d69f5810c761aa648de3ebd0b
khandelwalpranav05 <khandelwalpranav@rocketmail.com>
2019-11-21 19:02:10 +0530

    graph

--- Lecture 29/graph.cpp
+++ Lecture 29/graph.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <unordered_map>
 #include <list>
+#include <queue>
 
 using namespace std;
 
@@ -31,6 +32,32 @@ public:
 			cout<<endl;
 		}
 	}
+
+	void bfs(T src){
+		queue<T> q;
+		unordered_map<T,bool> visited;
+
+		visited[src] = true;
+		q.push(src);
+
+		while(!q.empty()){
+
+			T node = q.front();
+			q.pop();
+
+			cout<<node<<" ";
+
+			for(T neighbor:adjList[node]){
+				if(!visited[neighbor]){
+					q.push(neighbor);
+					visited[neighbor] = true;
+				}
+			}
+
+		}
+
+		cout<<endl;	
+	}
 };
 
 int main(){
@@ -43,13 +70,27 @@ int main(){
 
 	// g.display();
 
-	Graph<string> g;
-	g.addEdge("Coding Blocks","Java");
-	g.addEdge("Coding Blocks","Python");
-	g.addEdge("Coding Blocks","Django");
-	g.addEdge("Pranav","Coding Blocks");
+	// Graph<string> g;
+	// g.addEdge("Coding Blocks","Java");
+	// g.addEdge("Coding Blocks","Python");
+	// g.addEdge("Coding Blocks","Django");
+	// g.addEdge("Pranav","Coding Blocks");
+
+	// g.display();
+
+	Graph<int> g;
+
+	g.addEdge(1,2);
+	g.addEdge(1,4);
+	g.addEdge(2,3);
+	g.addEdge(3,4);
+	g.addEdge(3,5);
+	g.addEdge(5,6);
+	g.addEdge(5,4);
 
 	g.display();
+
+	g.bfs(1);
 
 	return 0;
 }
